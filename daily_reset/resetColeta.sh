a1e0a227c4cf617aa5aed8e8cffcc7a4983acbb2
Ubuntu <ubuntu@silberman.(none)>
2013-09-12 19:11:08 +0000

    fixing resetColeta.sh

--- daily_reset/resetColeta.sh
+++ daily_reset/resetColeta.sh
@@ -1,16 +1,20 @@
 #! /bin/bash
 
+# $1 = name of the hd partion
+# $2 = number of clients
+# $3 = log file
+
 #Mata o que 
 killall client.py
 killall server.py
 killall phantom.js
-mysql -u root -pOw,CEh24 pinteresTwitter < /data/haddock/pinterest-crawler/daily_reset/reset.sql
-nohup /data/haddock/pinterest-crawler/server.py &
+mysql -u root -pOw,CEh24 pinteresTwitter < /data/$1/pinterest-crawler/daily_reset/reset.sql
+nohup /data/$1/pinterest-crawler/server.py &
 
-for i in $( seq 20 )
+for i in $( seq $2 )
 do
-   nohup /data/haddock/pinterest-crawler/client.py localhost &
+   nohup /data/$1/pinterest-crawler/client.py localhost &
 done 
 
-echo "Reset at $(date):  $(echo $( mysql -u root -pOw,CEh24 pinterestTwitter -e "select count(*) from usersToCollect where statusColeta=2;") | cut -d" " -f2 ) users fully collected" >> $1
+echo "Reset at $(date):  $(echo $( mysql -u root -pOw,CEh24 pinterestTwitter -e "select count(*) from usersToCollect where statusColeta=2;") | cut -d" " -f2 ) users fully collected" >> $3
 
