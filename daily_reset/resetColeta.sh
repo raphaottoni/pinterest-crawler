5dc15ec3912d7e0965da5188e0111c53a5c7e3cd
Ubuntu <ubuntu@silberman.(none)>
2013-09-12 16:27:20 +0000

    debuggin

--- daily_reset/resetColeta.sh
+++ daily_reset/resetColeta.sh
@@ -9,8 +9,9 @@ killall client.py
 killall server.py
 killall phantom.js
 mysql -u root -pOw,CEh24 pinteresTwitter < /data/$1/pinterest-crawler/daily_reset/reset.sql
+sleep 1
 nohup /data/$1/pinterest-crawler/server.py &
-
+sleep 2
 for i in $( seq $2 )
 do
    nohup /data/$1/pinterest-crawler/client.py localhost &
