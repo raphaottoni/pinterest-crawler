37694dd80951d3660c2eda3826348b0313fccaaf
Ubuntu <ubuntu@herge.(none)>
2013-09-17 23:28:12 +0000

    bug fix in daily reset

--- daily_reset/resetColeta.sh
+++ daily_reset/resetColeta.sh
@@ -7,12 +7,12 @@
 echo "Reset at $(date):  $(echo $( mysql -u root -pOw,CEh24 pinterestTwitter -e "select count(*) from usersToCollect where statusColeta=2;") | cut -d" " -f2 ) users fully collected" >> $3
 #Mata o que 
 killall client.py
-#killall server.py
+killall server.py
 killall phantom.js
 mysql -u root -pOw,CEh24 pinterestTwitter < /data/$1/pinterest-crawler/daily_reset/reset.sql
-sleep 1
+sleep 10
 cd /data/$1/pinterest-crawler/
-#nohup /data/$1/pinterest-crawler/server.py &
+nohup /data/$1/pinterest-crawler/server.py &
 sleep 2
 for i in $( seq $2 )
 do
